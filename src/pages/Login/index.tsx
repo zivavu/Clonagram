d33840055935f4198b09e58f13f8c74181b17429
zivavu <zivavu@gmail.com>
2026-04-24 08:27:16 +0200

    Add prettier config

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,29 +1,29 @@
 import * as stylex from '@stylexjs/stylex';
 
 export default function LoginPage() {
-	const styles = stylex.create({
-		root: {
-			width: '100%',
-			height: '100%',
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-		},
-		leftSection: {
-			width: '100%',
-			height: '100%',
-		},
-		rightSection: {
-			minWidth: '650px',
-			maxWidth: '750px',
-			height: '100%',
-		},
-	});
+    const styles = stylex.create({
+        root: {
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        leftSection: {
+            width: '100%',
+            height: '100%',
+        },
+        rightSection: {
+            minWidth: '650px',
+            maxWidth: '750px',
+            height: '100%',
+        },
+    });
 
-	return (
-		<div {...stylex.props(styles.root)}>
-			<div {...stylex.props(styles.leftSection)}></div>
-			<div {...stylex.props(styles.rightSection)}></div>
-		</div>
-	);
+    return (
+        <div {...stylex.props(styles.root)}>
+            <div {...stylex.props(styles.leftSection)}></div>
+            <div {...stylex.props(styles.rightSection)}></div>
+        </div>
+    );
 }
