28f62f8462992a6eb8241d49b70b862d109344cd
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-05-29 15:34:56 +0200

    Improve CarouselArrow styling

--- src/components/CarouselArrow/index.stylex.ts
+++ src/components/CarouselArrow/index.stylex.ts
@@ -4,10 +4,15 @@ import { radius } from '../../styles/tokens.stylex';
 export const styles = stylex.create({
    root: {
       position: 'absolute',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
       top: '50%',
       transform: 'translateY(-50%)',
       zIndex: 2,
       borderRadius: radius.full,
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+      boxShadow: '0 0 4px 1px rgba(0, 0, 0, 0.2)',
    },
    left: {
       left: '12px',
