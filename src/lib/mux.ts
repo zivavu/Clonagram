a091be0ed7a582f031dbadef80c9db7e828e1f44
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-06-03 18:18:19 +0200

    Remove mux singleton antipattern

--- src/lib/mux.ts
+++ src/lib/mux.ts
@@ -1,16 +1,11 @@
 import Mux from '@mux/mux-node';
 
-let _muxClient: Mux | null = null;
-
 export function getMuxClient(): Mux {
-   if (_muxClient) return _muxClient;
-
    const muxTokenId = process.env.MUX_TOKEN_ID;
    const muxTokenSecret = process.env.MUX_TOKEN_SECRET;
    if (!muxTokenId || !muxTokenSecret) {
       throw new Error('Missing MUX_TOKEN_ID or MUX_TOKEN_SECRET');
    }
 
-   _muxClient = new Mux({ tokenId: muxTokenId, tokenSecret: muxTokenSecret });
-   return _muxClient;
+   return new Mux({ tokenId: muxTokenId, tokenSecret: muxTokenSecret });
 }
